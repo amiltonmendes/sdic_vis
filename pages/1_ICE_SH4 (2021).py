7d5c3cfb94b63c4e17fb8c8f5d182a2f00afdd92
Amilton Lobo <amilton.mendes@gmail.com>
2023-08-21 13:08:47 -0300

    Adição de crescimento e normalização

--- pages/1_ICE_SH4 (2021).py
+++ pages/1_ICE_SH4 (2021).py
@@ -14,8 +14,8 @@ st.set_page_config(layout="wide")
 @st.cache_data
 def load_data():
     locale._override_localeconv = {'thousands_sep': '.'}
-    retorno = pd.read_csv('./raw_data_ice2021_pgi_pei.csv')
-    retorno['complementar_pgi'] = 1-retorno['pgi']
+    retorno = pd.read_csv('./raw_data_pegi_pei_growth.csv')
+    
     retorno = retorno[retorno['rca']<1]
     return retorno
 
@@ -32,6 +32,14 @@ if 'slider_Oportunidades' not in st.session_state:
     st.session_state['slider_Oportunidades'] = (33,66)
 if 'slider_Ganhos' not in st.session_state:
     st.session_state['slider_Ganhos'] = 50
+if 'peso_crescimento' not in st.session_state:
+    st.session_state['peso_crescimento'] = 0.25
+if 'peso_importacao_global' not in st.session_state:
+    st.session_state['peso_importacao_global'] = 0.25
+if 'peso_importacao' not in st.session_state:
+    st.session_state['peso_importacao'] = 0.25
+if 'peso_dcr' not in st.session_state:
+    st.session_state['peso_dcr'] = 0.25
 
 
 row = st.columns([1,7])
@@ -40,19 +48,32 @@ with row[1].expander("Capacidades atuais"):
     header = st.columns([1,1,1,2])
     #header[0].markdown('#### Capacidades Atuais')
     header[1].markdown('<div style="text-align: right;">Valor exportado: '+str(float(st.session_state['slider_CapacidadesAtuais'][0]/100))+'</div>',unsafe_allow_html=True)
-    header[1].markdown('<div style="text-align: right;">Vantagens comparativas relativas: '+str(float((st.session_state['slider_CapacidadesAtuais'][1]-st.session_state['slider_CapacidadesAtuais'][0])/100))+'</div>',unsafe_allow_html=True)
+    header[1].markdown('<div style="text-align: right;">Vantagens comparativas revelada: '+str(float((st.session_state['slider_CapacidadesAtuais'][1]-st.session_state['slider_CapacidadesAtuais'][0])/100))+'</div>',unsafe_allow_html=True)
     header[1].markdown('<div style="text-align: right;">Densidade do produto: '+str(float((100-st.session_state['slider_CapacidadesAtuais'][1])/100)) +'</div>',unsafe_allow_html=True)
+    
     sliderCapacidades = header[3].slider('Capacidades.', 0, 100, key='slider_CapacidadesAtuais', label_visibility='hidden')
 
 row = st.columns([1,7])
 peso_oportunidaes=row[0].number_input('Oportunidades',min_value=0.0,max_value=1.0,value=0.2,label_visibility='collapsed')
 with row[1].expander("Oportunidades de mercado"):
-    header = st.columns([1,1,1,2])
-    #header[0].markdown('#### Oportunidades de mercado')
-    header[1].markdown('<div style="text-align: right;">Valor importado: '+str(float(st.session_state['slider_Oportunidades'][0]/100))+'</div>',unsafe_allow_html=True)
-    header[1].markdown('<div style="text-align: right;">Valor importado (Mundo): '+str(float((st.session_state['slider_Oportunidades'][1]-st.session_state['slider_Oportunidades'][0])/100))+'</div>',unsafe_allow_html=True)
-    header[1].markdown('<div style="text-align: right;">Desvantagem comparativa relativa: '+str(float((100-st.session_state['slider_Oportunidades'][1])/100))+'</div>',unsafe_allow_html=True)
-    sliderOportunidades = header[3].slider('Oportunidades.', 0, 100, key='slider_Oportunidades', label_visibility='hidden')
+    header = st.columns([1,1,1,1,1])
+    header[1].markdown('<div style="text-align: right;">Valor importado: '+str(float(st.session_state['peso_importacao']))+'</div>',unsafe_allow_html=True)
+    header[3].number_input('Importacao',min_value=0.0,max_value=1.0,key='peso_importacao',label_visibility='collapsed')
+
+    header = st.columns([1,1,1,1,1])
+    header[1].markdown('<div style="text-align: right;">Valor importado (Mundo): '+str(float(st.session_state['peso_importacao_global']))+'</div>',unsafe_allow_html=True)
+    header[3].number_input('ImportacaoGlobal',min_value=0.0,max_value=1.0,key='peso_importacao_global',label_visibility='collapsed')
+
+
+    header = st.columns([1,1,1,1,1])
+    header[1].markdown('<div style="text-align: right;">Desvantagem comparativa relativa: '+str(float(st.session_state['peso_dcr']))+'</div>',unsafe_allow_html=True)
+    header[3].number_input('DCR',min_value=0.0,max_value=1.0,key='peso_dcr',label_visibility='collapsed')
+
+
+    header = st.columns([1,1,1,1,1])
+    header[1].markdown('<div style="text-align: right;">Crescimento: '+str(float(st.session_state['peso_crescimento']))+'</div>',unsafe_allow_html=True)
+    header[3].number_input('Crescimento',min_value=0.0,max_value=1.0,key='peso_crescimento',label_visibility='collapsed')
+    #sliderOportunidades = header[3].slider('Oportunidades.', 0, 100, key='slider_Oportunidades', label_visibility='hidden')
 
 row = st.columns([1,2,5])
 peso_pgi=row[0].number_input('PGI',min_value=0.0,max_value=1.0,value=0.2,label_visibility='collapsed')
@@ -66,7 +87,7 @@ row[1].text('Product Emission Intensity index')
 row = st.columns([1,7])
 peso_ganhos=row[0].number_input('Ganhos',min_value=0.0,max_value=1.0,value=0.3,label_visibility='collapsed')
 
-with row[1].expander("Análise de ganhos"):
+with row[1].expander("Ganhos de complexidade"):
     header = st.columns([1,1,1,2])
     #header[0].markdown('#### Análise de ganhos')
     header[1].markdown('<div style="text-align: right;">Índice de Complexidade do Produto: '+str(float(st.session_state['slider_Ganhos']/100))+'</div>',unsafe_allow_html=True)
@@ -85,9 +106,10 @@ peso_densidade_produto = (100-st.session_state['slider_CapacidadesAtuais'][1])/1
 #peso_capacidade_atuais = 0.33
 
 #Oportunidades de mercado
-peso_valor_importado = st.session_state['slider_Oportunidades'][0]/100
-peso_valor_importado_total = (st.session_state['slider_Oportunidades'][1]-st.session_state['slider_Oportunidades'][0])/100
-peso_dcr = (100-st.session_state['slider_Oportunidades'][1])/100
+peso_valor_importado = st.session_state['peso_importacao']
+peso_valor_importado_total = st.session_state['peso_importacao_global']
+peso_dcr = st.session_state['peso_dcr']
+peso_crescimento = st.session_state['peso_crescimento']
 
 #peso_oportunidaes = 0.33
 
@@ -101,12 +123,12 @@ peso_igo = (100-st.session_state['slider_Ganhos'])/100
 df['valor_indice'] = peso_capacidade_atuais*(peso_valor_exportado*df['export_value_standarized'] + \
                                                               peso_vcr*df['rca_standarized'] + peso_densidade_produto*df['density_standarized'])+\
     peso_oportunidaes*(peso_valor_importado*df['import_value_standarized'] + peso_valor_importado_total*df['import_value_total_standarized']\
-                   + peso_dcr*df['rcd_standarized']) + \
+                   + peso_dcr*df['rcd_standarized'] +peso_crescimento*df['growth_standarized'] ) + \
      peso_ganhos*(peso_complexidade_produto*df['pci_standarized'] + peso_igo*df['cog_standarized'])  + peso_pgi*df['pgi_standirized']   \
         + peso_pei*df['pei_inverted']
 
 
-df_plot = df[['hs_product_code','hs_product_name_short_en','export_value','rca','density','import_value','import_value_total','rcd','pci','cog','valor_indice','pgi','pei']]
+df_plot = df[['valor_indice','hs_product_code','hs_product_name_short_en','export_value','rca','growth','density','import_value','import_value_total','rcd','pci','cog','pgi','pei']]
 
 
 df_plot['hs_product_code'] = df_plot['hs_product_code'].apply(str)
@@ -122,6 +144,10 @@ df_plot['pgi'] = df_plot['pgi'].map('{:.2f}'.format)
 df_plot['export_value'] = df_plot['export_value']/1000000
 df_plot['export_value'] = df_plot['export_value'].map('${:,.2f}'.format)
 
+df_plot['growth'] = df_plot['growth']/1000000
+df_plot['growth'] = df_plot['growth'].map('${:,.2f}'.format)
+
+
 df_plot['import_value'] = df_plot['import_value']/1000000
 df_plot['import_value'] = df_plot['import_value'].map('${:,.2f}'.format)
 
@@ -134,11 +160,12 @@ df_plot['pei'] = df_plot['pei'].map('{:,.2f}'.format)
 
 
 df_plot['rank'] = df_plot['valor_indice'].rank(method='dense',ascending=False)
+df_plot = df_plot[['rank','valor_indice','hs_product_code','hs_product_name_short_en','export_value','rca','density','import_value','import_value_total','growth','rcd','pci','cog','pgi','pei']]
 
 filtro_sh4 = st.text_input('Digite o SH4 desejado:')
 df_plot = df_plot[df_plot['hs_product_code'].str.startswith(filtro_sh4)]
 
-df_plot = df_plot.rename(columns={'hs_product_code' : ' Código HS 2007','hs_product_name_short_en': 'Descrição', 'export_value' : 'Exp (Milhões)', 'import_value' : 'Imp. (Milhões)','import_value_total' : 'Imp. Mundo (Milhões)', 'rca' : 'VCR', 'density' : 'Dens.', 'rcd' : 'DCR', 'pci' : 'ICP','cog' : 'Ganho de Op.','rank' : 'Rank','pgi' : 'PGI', 'pei' : 'PEI (Mil)'} )
+df_plot = df_plot.rename(columns={'hs_product_code' : ' Código HS 2007','hs_product_name_short_en': 'Descrição', 'export_value' : 'Exp (Milhões)','growth' : 'Crescimento (Milhões 2013-2021)', 'import_value' : 'Imp. (Milhões)','import_value_total' : 'Imp. Mundo (Milhões)', 'rca' : 'VCR', 'density' : 'Dens.', 'rcd' : 'DCR', 'pci' : 'ICP','cog' : 'Ganho de Op.','rank' : 'Rank','pgi' : 'PGI', 'pei' : 'PEI (Mil)'} )
 
 
 st.dataframe(df_plot.drop('valor_indice',axis=1).sort_values(by='Rank'), use_container_width=True,hide_index =True)
