94cd240baa15531b2e5e2293aafb1de04d8c6f12
Amilton Lobo <amilton.mendes@gmail.com>
2023-08-31 18:27:50 -0300

    Explicações, exceto externalidades

--- pages/2_ICE_SH4 (2021) Bloco América do Sul Novo.py
+++ pages/2_ICE_SH4 (2021) Bloco América do Sul Novo.py
@@ -27,41 +27,41 @@ tab1, tab2, tab3, tab4 = st.tabs(["Capacidades atuais", "Oportunidades", "Ganhos
 with tab1:
 
     row = st.columns([2,1,4])
-    row[0].markdown('#### <div style="text-align: right;">Capacidades atuais</div>',unsafe_allow_html=True,help="Teste")
+    row[0].markdown('#### <div style="text-align: right;">Capacidades atuais</div>',unsafe_allow_html=True,help="Peso dado ao grupo das variáveis abaixo no cálculo do ranking")
     peso_capacidade_atuais = row[1].number_input('capacidade',min_value=0.2,max_value=0.4,value=0.3,label_visibility='collapsed')
     row = st.columns([2,1,4])
-    row[0].markdown('##### <div style="text-align: right;">Valor exportado</div>',unsafe_allow_html=True)
+    row[0].markdown('##### <div style="text-align: right;">Valor exportado</div>',unsafe_allow_html=True, help="Peso do valor das exportações brasileiras do produto")
     peso_valor_exportado = row[1].number_input('Exportação',min_value=0.0,max_value=0.5,value=0.33,label_visibility='collapsed')
 
     row = st.columns([2,1,4])
-    row[0].markdown('##### <div style="text-align: right;">Vantagem comparativa revelada</div>',unsafe_allow_html=True)
+    row[0].markdown('##### <div style="text-align: right;">Vantagem comparativa revelada</div>',unsafe_allow_html=True, help="""Peso da vantagem comparativa revelada (VCR).\nValores de VCR acima de 1 indicam que o produto representa mais para a pauta exportadora brasileira do que a média mundial, ou seja, o país é competitivo em relação a esse produto.""")
     peso_vcr = row[1].number_input('vcr',min_value=0.0,max_value=0.5,value=0.33,label_visibility='collapsed')
 
     row = st.columns([2,1,4])
-    row[0].markdown('##### <div style="text-align: right;">Densidade do produto</div>',unsafe_allow_html=True)
+    row[0].markdown('##### <div style="text-align: right;">Densidade do produto</div>',unsafe_allow_html=True,help="Peso da densidade do produto.\nValores mais altos de densidade indicam uma maior proximidade de determinado produto com a estrutura produtiva atual do país do que outros com valores de densidade mais baixos.")
     peso_densidade_produto = row[1].number_input('densidade',min_value=0.0,max_value=0.8,value=0.33,label_visibility='collapsed')
 
 
 with tab2:
     row = st.columns([2,1,4])
-    row[0].markdown('#### <div style="text-align: right;">Oportunidades</div>',unsafe_allow_html=True)
+    row[0].markdown('#### <div style="text-align: right;">Oportunidades</div>',unsafe_allow_html=True,help="Peso dado ao grupo das variáveis abaixo no cálculo do ranking.")
     peso_oportunidaes=row[1].number_input('Oportunidades',min_value=0.2,max_value=0.4,value=0.3,label_visibility='collapsed')
 
     row = st.columns([2,1,4])
-    row[0].markdown('##### <div style="text-align: right;">Valor importado</div>',unsafe_allow_html=True)
+    row[0].markdown('##### <div style="text-align: right;">Valor importado</div>',unsafe_allow_html=True,help="Peso das importações brasileiras do produto")
     peso_importacao = row[1].number_input('peso_importacao',min_value=0.0,max_value=1.0,value=0.25,label_visibility='collapsed')
 
 
     row = st.columns([2,1,4])
-    row[0].markdown('##### <div style="text-align: right;">Valor importado (Mundo)</div>',unsafe_allow_html=True)
+    row[0].markdown('##### <div style="text-align: right;">Valor importado (Mundo)</div>',unsafe_allow_html=True,help="Peso das importações mundiais do produto")
     peso_importacao_global = row[1].number_input('Importacao Global',min_value=0.0,max_value=1.0,value=0.25,label_visibility='collapsed')
 
     row = st.columns([2,1,4])
-    row[0].markdown('##### <div style="text-align: right;">Desvantagem comparativa revelada</div>',unsafe_allow_html=True)
+    row[0].markdown('##### <div style="text-align: right;">Desvantagem comparativa revelada</div>',unsafe_allow_html=True,help="Peso da desvantagem comparativa revelada (DCR).\nValores de DCR acima de 1 indicam que o produto representa mais para a pauta importadora brasileira do que a média mundial, ou seja, o país é dependente de importações em relação a esse produto.")
     peso_dcr = row[1].number_input('DCR',min_value=0.0,max_value=1.0,value=0.25,label_visibility='collapsed')
 
     row = st.columns([2,1,4])
-    row[0].markdown('##### <div style="text-align: right;">Crescimento de importações (2021 - 2013)</div>',unsafe_allow_html=True)
+    row[0].markdown('##### <div style="text-align: right;">Crescimento de importações (2021 - 2013)</div>',unsafe_allow_html=True,help="Peso do crescimento das importações mundiais do produto, calculado a partir da subtração das importações totais do ano de 2021 pelas do ano de 2013.")
     peso_crescimento = row[1].number_input('Crescimento',min_value=0.0,max_value=1.0,value=0.1,label_visibility='collapsed')
 
 
@@ -69,15 +69,15 @@ with tab2:
 
 with tab3:
     row = st.columns([2,1,4])
-    row[0].markdown('#### <div style="text-align: right;">Ganhos de complexidade</div>',unsafe_allow_html=True)
+    row[0].markdown('#### <div style="text-align: right;">Ganhos de complexidade</div>',unsafe_allow_html=True,help="Peso dado ao grupo de variáveis abaixo no cálculo do ranking.")
     peso_ganhos=row[1].number_input('Ganhos',min_value=0.4,max_value=0.8,value=0.5,label_visibility='collapsed')
 
     row = st.columns([2,1,4])
-    row[0].markdown('##### <div style="text-align: right;">Índice de Complexidade do Produto</div>',unsafe_allow_html=True)
+    row[0].markdown('##### <div style="text-align: right;">Índice de Complexidade do Produto</div>',unsafe_allow_html=True, help="Peso do índice de complexidade do produto (ICP). Mede a diversidade e a sofisticação da expertise necessária para fabricar um produto. O ICP é calculado a partir de quantos outros países podem fabricar o produto, assim como a complexidade econômica desses países.")
     peso_indice_complexidade = row[1].number_input('Índice complexidade',min_value=0.3,max_value=0.8,value=0.5,label_visibility='collapsed')
 
     row = st.columns([2,1,4])
-    row[0].markdown('##### <div style="text-align: right;">Índice de Ganho de Oportunidade</div>',unsafe_allow_html=True)
+    row[0].markdown('##### <div style="text-align: right;">Índice de Ganho de Oportunidade</div>',unsafe_allow_html=True,help="Peso do índice de ganho de oportunidade. O índice de ganho de oportunidade mede o quanto um país pode se beneficiar com a diversificação futura a partir do desenvolvimento de um produto em particular. Dessa forma, o valor estratégico de um produto é baseado nas possibilidades de diversificação para setores mais complexos que são abertas.")
     peso_indice_ganho_oportunidade = row[1].number_input('Ganho oportunidade',min_value=0.3,max_value=0.8,value=0.5,label_visibility='collapsed')
 
 
@@ -117,7 +117,7 @@ componente_ganhos = (peso_ganhos/(peso_indice_ganho_oportunidade+peso_indice_com
 
 #Criar o indice de impacto AMS
 df['impacto_ams'] = 1 - df['proporcao_importacao_origem_brasil']
-df.loc[df['dcr_bloco']<1,'impacto_ams'] = 0
+df.loc[df['dcr_bloco']<=1,'impacto_ams'] = 0
 
 componentes_externalidades = peso_externalidades/(peso_impacto_ams+peso_pei+peso_pgi)*(peso_pei*df['pei_normalized_inverted'] + peso_pgi*df['pgi_normalized_inverted'] + peso_impacto_ams*df['impacto_ams'])
 
---
+++ pages/3_ICE_Conceitos básicos.py
@@ -0,0 +1,13 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import pandas as pd
+import streamlit as st
+import pandas as pd
+import locale
+
+
+st.set_page_config(layout="wide")
+
